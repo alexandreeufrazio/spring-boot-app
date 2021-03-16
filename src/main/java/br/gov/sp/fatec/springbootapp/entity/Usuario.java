018d037c96bb5d7f09658edb11e9c83d52d543af
alexandreeufrazio <alee.ram@hotmail.com>
2021-03-16 00:58:44 +0000

    15032021-3

---
+++ src/main/java/br/gov/sp/fatec/springbootapp/entity/Usuario.java
@@ -0,0 +1,38 @@
+package br.gov.sp.fatec.springbootapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = usr_usuario)
+public class Usuario {
+    
+    private long id;
+    private String nome;
+    private String senha;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
